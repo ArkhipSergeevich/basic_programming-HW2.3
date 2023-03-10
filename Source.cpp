feac19cf1e4d7d175763f33b0c08baabb70c57ae
Arkhip Popov <arhippopov0509@yandex.ru>
2023-03-10 13:49:01 +0300

    fixed task3

--- Source.cpp
+++ Source.cpp
@@ -9,17 +9,17 @@ struct adress
 	int flat_number;
 	int index;
 };
-void output_function(adress& country1, adress& city1, adress& street1, adress& home_number1, adress& flat_number1, adress& index1)
+void output_function(const adress& adress) 
 {
 	std::cout << "Ваш адрес:" << std::endl;
-	std::cout << country1.country << " " << city1.city << " " << street1.street << " " << home_number1.home_number << " " << flat_number1.flat_number << " " << index1.index << std::endl;
+	std::cout << adress.country << " " << adress.city << " " << adress.street << " " << adress.home_number << " " << adress.flat_number << " " << adress.index << std::endl;
 };
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	adress first = { "Russia", "Moscow", "Arbat", "3a", 10, 29387 };
-	output_function(first, first, first, first, first, first);
+	const adress first = { "Russia", "Moscow", "Arbat", "3a", 10, 29387 };
+	output_function(first); 
 }
 
 
